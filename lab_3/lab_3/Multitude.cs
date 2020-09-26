63c280ff40b4e2c08f01a4717e12c91eb8d9a1bb
evgeniy <vkzheny@gmail.com>
2020-09-26 14:15:45 +0300

    partial

--- lab_3/lab_3/Multitude.cs
+++ lab_3/lab_3/Multitude.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace lab_3
 {
-    public class Multitute : IEnumerable
+    public partial class Multitute : IEnumerable
     {
 
         #region Поля и Свойства
@@ -57,100 +57,6 @@ namespace lab_3
             _elementsCount = 0;
         }
         
-        #endregion
-
-        #region Методы
-
-        public void Add(int element)
-        {
-            if (Exist(element))
-                throw new Exception($"element {element} already exist");
-            
-            if(Length == Capacity)
-                throw new Exception("Array is full");
-            
-            else
-            {
-                _multitudes[Length] = element;
-                Length++;
-            }
-        }
-
-        public void Delete(int num)
-        {
-            for (int i = 0; i < Length; i++)
-            {
-                if (_multitudes[i].ToString() == num.ToString())
-                {
-                    for (var j = i; j < Length - 1; j++)
-                    {
-                        _multitudes[j] = _multitudes[j + 1];
-                    } 
-                    Length--;
-                    return;
-                }       
-            }
-        }
-        public void DeleteIndex(int index)
-        {
-            for (var j = index; j < Length - 1; j++)
-            {
-                    _multitudes[j] = _multitudes[j + 1];
-            } 
-            Length--;
-        }
-
-        public static Multitute Create(int capacity = 1024)
-        {
-            return new Multitute(capacity);
-        }
-
-        public override string ToString()
-        {
-            var str = _multitudes.Aggregate("{ ", (current, mlt) => current + $"{mlt} ") + " }";
-            
-            return str;
-        }
-
-        public bool Equals(Multitute obj)
-        {
-            if (obj == null || obj.Length != Length)
-                return false;
-            
-            var i = 0;
-            foreach (var m in obj){
-                if (!_multitudes[i++].Equals(m))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            int hash = 0;
-
-            for (int i = 0; i < ID; i++)
-            {
-                hash += ID;
-                hash += (hash << 10);
-                hash ^= (hash >> 6);
-            }
-            
-            hash += (hash << 3);
-            hash ^= (hash >> 11);
-            hash += (hash << 15);
-            
-            return (hash < 0 ? Math.Abs(hash) : hash);
-        }
-
-        private bool Exist(int element)
-        {
-            return _multitudes.Any(mlt => mlt.ToString() == element.ToString());
-        }
-
         #endregion
 
         #region Enum
